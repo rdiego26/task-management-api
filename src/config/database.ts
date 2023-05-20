c8518b3b9bdd0528da471bacddca93410bbe4a63
ramos <rdiego26@gmail.com>
2023-05-20 16:49:09 +0100

    chore: add typeorm

---
+++ src/config/database.ts
@@ -0,0 +1,15 @@
+import { ConnectionOptions } from "typeorm";
+import { User } from "../entities/User";
+
+const config: ConnectionOptions = {
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [User],
+  synchronize: true,
+};
+
+export default config;
