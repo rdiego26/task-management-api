af65ee9411d1ee31cd8c8c7cdcf856bfe1164ba7
ramos <rdiego26@gmail.com>
2023-05-21 18:44:17 +0100

    feat: add task entity

---
+++ src/entities/Task.ts
@@ -0,0 +1,40 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    DeleteDateColumn,
+    JoinColumn,
+    OneToOne
+} from "typeorm";
+import { Length } from "class-validator";
+import { User } from "./User";
+
+@Entity()
+export class Task {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @Column({
+        nullable: false
+    })
+    @Length(0, 2500)
+    summary!: string;
+
+    @Column()
+    performedAt!: Date;
+
+    @OneToOne(() => User)
+    @JoinColumn()
+    owner!: User;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date;
+
+    @DeleteDateColumn()
+    deletedAt!: Date;
+}
